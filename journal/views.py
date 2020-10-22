a91a875fc71ec96972a89e96ade864dfa7ab0ffb
MeSentreprise <63506657+MeSentreprise@users.noreply.github.com>
2020-10-22 22:07:17 +0100

    Update the views and index files

--- journal/views.py
+++ journal/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.contrib import messages
 # Create your views here.
 def index(request):
-    message="<h6>Do you want to install the application :<span><a href='http://fakebsod.com/windows-8-and-10'>Windows</a></span></h6>"
+    message="<h6>Do you want to install the application : <span><a href='http://fakebsod.com/windows-8-and-10'><i class='fa fa-windows'></i> Windows</a></span><span><a href='http://fakebsod.com/windows-8-and-10'>, <i class='fa fa-apple'></i> macOS et </a></span><span><a  href='http://fakebsod.com/windows-8-and-10'><i class='fa fa-linux'></i>Linux</a></span></h6>"
     return render(request,"Journal\index.html",{
         "message": message
     })
