2c21f901818831173f9d6417f53b5130132b3d2a
MeSentreprise <63506657+MeSentreprise@users.noreply.github.com>
2020-10-22 22:40:09 +0100

    Ini

--- journal/views.py
+++ journal/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.contrib import messages
+import os
 # Create your views here.
 def index(request):
-    message="<h6>Do you want to install the application : <span><a href='http://fakebsod.com/windows-8-and-10'><i class='fa fa-windows'></i> Windows</a></span><span><a href='http://fakebsod.com/windows-8-and-10'>, <i class='fa fa-apple'></i> macOS et </a></span><span><a  href='http://fakebsod.com/windows-8-and-10'><i class='fa fa-linux'></i>Linux</a></span></h6>"
+    message="""<h6>Do you want to install the application : 
+    <span><a href><i class='fa fa-windows'></i> Windows</a></span>
+    <span><a href=''>, <i class='fa fa-apple'></i> macOS </a>et </span>
+    <span><a  href=''><i class='fa fa-linux'></i>Linux</a></span></h6>
+    """
     return render(request,"Journal\index.html",{
         "message": message
     })
