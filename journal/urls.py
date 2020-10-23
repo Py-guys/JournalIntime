1b5a34cadefdfcd74bf81f48e09a26493ee035ad
MeSentreprise <63506657+MeSentreprise@users.noreply.github.com>
2020-10-23 17:09:02 +0100

    A

--- journal/urls.py
+++ journal/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from journal import views
 
 urlpatterns = [
-    path("",views.index,name="index"),
-    path('/downloads', views.download_file, name="Wouhoo")
+    path("",views.index,name="index")
 ]
--- journal/views.py
+++ journal/views.py
@@ -13,13 +13,3 @@ def index(request):
     return render(request,"Journal/index.html",{
         "message": message
     })
-def download_file(request):
-    # fill these variables with real values
-    fl_path = 'Downloads'
-    filename = 'Haha.txt'
-
-    fl = open(fl_path, 'r')
-    mime_type, _ = mimetypes.guess_type(fl_path)
-    response = HttpResponse(fl, content_type=mime_type)
-    response['Content-Disposition'] = "attachment; filename=%s" % filename
-    return response
