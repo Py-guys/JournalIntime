26bf0a98f0552c93b7b9a81978e27a5997a98f53
MeSentreprise <63506657+MeSentreprise@users.noreply.github.com>
2020-10-23 17:04:44 +0100

    haha

--- journal/urls.py
+++ journal/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from journal import views
 
 urlpatterns = [
-    path("",views.index,name="index")
+    path("",views.index,name="index"),
+    path('/downloads', views.download_file, name="Wouhoo")
 ]
