bd9dff004a0bb00d8107e23295e36f8579aea8fd
MeSentreprise <63506657+MeSentreprise@users.noreply.github.com>
2020-10-25 20:57:21 +0100

    Haah coico

--- journal/urls.py
+++ journal/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from journal import views
 
 urlpatterns = [
-    path("",views.index,name="index")
+    path("",views.index,name="index"),
 ]
--- journal/views.py
+++ journal/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render, HttpResponse
 import mimetypes
 from django.contrib import messages
 import os
+
 # Create your views here.
 def index(request):
     message="""<h6>Do you want to install the application : 
-    <span><a href><i class='fa fa-windows'></i> Windows</a></span>
-    <span><a href='Downloads\Haha.txt' download="Hamimetypes
-    ha.txt">, <i class='fa fa-apple'></i> macOS </a>et </span>
-    <span><a  href='/Haha.txt'><i class='fa fa-linux'></i>Linux</a></span></h6>
+    <span><a href="https://github.com/MeSentreprise/JournalIntime/releases/download/1.0/git-bash.exe"><i class='fa fa-windows'></i> Windows</a></span>
+	<pan><a href="https://github.com/MeSentreprise/JournalIntime/releases/download/1.0/git-bash.exe">,<i class='fa fa-apple'></i> macOS </a>et </span>
+    <span><a href="https://github.com/MeSentreprise/JournalIntime/releases/download/1.0/git-bash.exe"><i class='fa fa-linux'></i>Linux</a></span></h6>
     """
     return render(request,"Journal/index.html",{
         "message": message
